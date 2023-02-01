443f6a40664156c284d825f04b34954263332e4e
atlantageek <atlantageek@gmail.com>
2023-02-01 06:13:29 -0500

    adding connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,18 +3,11 @@ module ApplicationCable
 
     def connect
       Rails.logger.debug(find_verified_user.to_s)
-      self.currentx_user = find_verified_user
+      self.current_user = find_verified_user
     end
 
     private
-      # def find_verified_user
-      #   logger.debug(self.current_user)
-      #   if verified_user = User.find_by(id: cookies.encrypted[:user_id])
-      #     verified_user
-      #   else
-      #     reject_unauthorized_connection
-      #   end
-      # end
+
       def find_verified_user
         current_user = User.find_by(id: cookies.signed[:user_id])
         Rails.logger.debug(cookies.signed[:user_id])
