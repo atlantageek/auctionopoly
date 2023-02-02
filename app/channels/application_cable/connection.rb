f9b175bb4242ada04a3bdb557a3e340dda46bedc
atlantageek <atlantageek@gmail.com>
2023-02-02 07:04:00 -0500

    fixing deviser cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,17 +2,17 @@ module ApplicationCable
   class Connection < ActionCable::Connection::Base
 
     def connect
-      Rails.logger.debug(find_verified_user.to_s)
-      self.current_user = find_verified_user
+      #Rails.logger.debug(find_verified_user.to_s)
+      @current_user = find_verified_user
     end
 
     private
 
       def find_verified_user
-        current_user = User.find_by(id: cookies.signed[:user_id])
-        Rails.logger.debug(cookies.signed[:user_id])
-        if current_user
-          current_user
+        @current_user = User.find_by(id: cookies.signed[:user_id])
+        #Rails.logger.debug(cookies.signed[:user_id])
+        if @current_user
+          @current_user
         else
           reject_unauthorized_connection
         end
